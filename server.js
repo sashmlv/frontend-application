21d5dbfd48b398aaecbe342305960ee955b9aeb1
asus <sashmlv@yandex.ru>
2020-10-22 22:26:59 +0300

    Update handle template.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require( 'express' ),
    server = require( 'http' ).createServer( app ),
    ROOT = __dirname;
 
-if( ! fs.existsSync( `${ROOT}/config.js` )) {
+if( ! fs.existsSync( `${ ROOT }/config.js` )) {
 
    throw new Error( 'Config not found' );
 };
@@ -19,85 +19,64 @@ const {
    NODE_ENV,
    SPA,
    SSR,
-} = require( `${ROOT}/config` );
+} = require( `${ ROOT }/config` );
 
 const data = { appName: 'dashboard' };
 
-if( SSR && SPA || !SSR && !SPA ) {
+if( SSR && SPA || ! SSR && ! SPA ) {
 
    throw new Error( 'Please set config parameter for buld SPA or SSR' );
 };
 
-const index = SSR ? require( `${ROOT}/dist/server` ).default : undefined;
+const index = SSR ? require( `${ ROOT }/dist/server` ).default : undefined;
 
-if( SSR && ! fs.existsSync( `${ROOT}/dist/server` )) {
+if( SSR && ! fs.existsSync( `${ ROOT }/dist/server` )) {
 
    throw new Error( 'Please build server application before' );
 };
 
-// template = fs.readFileSync();
-
 server.on( 'error', err => console.log( err ));
 
-const serve = SPA ? `${ROOT}/dist` : SSR ? `${ROOT}/dist/client` : undefined;
+const serve = SPA ? `${ ROOT }/dist` : SSR ? `${ ROOT }/dist/client` : undefined,
+   templateStr = fs.readFileSync( `${ ROOT }/dist/index.html`, 'utf8' );
 
-( async _=> {
+/* get template parts */
+let parts = templateStr.split( '<!--HEAD-->' );
 
-   app.use( '/', express.static( serve ));
+const beforeHead = parts[ 0 ];
 
-   app.get( '*', ( req, res, next ) => {
+parts = parts[ 1 ].split( '<!--STYLE-->' );
 
-      console.log( req.url );
+const beforeStyle = parts[ 0 ];
 
-      if( SPA ) {
+parts = parts[ 1 ].split( '<!--HTML-->' );
 
-         res.send(`
-<!doctype html>
-<html>
-<head>
-   <meta charset='utf8'>
-   <meta name='viewport' content='width=device-width'>
-   <title>ssr app</title>
-</head>
-<body>
-   <div id='app'></div>
-   <script src='index.js'></script>
-</body>
-</html>
-      `);
-      }
-      else if( SSR ) {
+const beforeHtml = parts[ 0 ],
+   afterHtml = parts[ 1 ];
 
-         const { head, html, css, } = index.render({ url: req.url, ...data });
+// app.use( '/', express.static( serve ));
+app.use( express.static( serve ));
 
-         res.send(`
-<!doctype html>
-<html>
-<head>
-   <meta charset='utf8'>
-   <meta name='viewport' content='width=device-width'>
-   <title>ssr app</title>
-   ${head}
-</head>
-<body>
-   <style>
-      ${css && css.code ? css.code : ''}
-   </style>
-   <div id='app'>
-      ${html}
-   </div>
-   <script src='index.js'></script>
-</body>
-</html>
-      `);
-      }
+app.get( '*', ( req, res, next ) => {
 
-      return next();
-   });
+   console.log( req.url, req.baseUrl );
 
-   server.listen(
-      PORT,
-      HOST,
-      _=> console.log(`Server listen at: ${HOST}:${PORT}, NODE_ENV:${NODE_ENV}`),
-   );
-})();
+   if( SPA ) {
+
+      res.send( `${ beforeHead }${ beforeStyle }${ beforeHtml }${ afterHtml }`);
+   }
+   else if( SSR ) {
+
+      const { head, html, css, } = index.render({ url: req.url, ...data });
+
+      res.send( `${ beforeHead }${ head }${ beforeStyle }${ css && css.code ? css.code : '' }${ beforeHtml }${ html }${ afterHtml }`);
+   }
+
+   return next();
+});
+
+server.listen(
+   PORT,
+   HOST,
+   _=> console.log( `Server listen at: ${ HOST }:${ PORT }, NODE_ENV: ${ NODE_ENV }`),
+);
